ee28d3c2e25bed9e09a97e3e259b7bd7dd23657d
Julio Capote <jcapote@gmail.com>
2011-11-02 10:42:11 -0700

    benchmark util script

---
+++ misc/bench.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'typhoeus'
+
+
+#response = Typhoeus::Request.get("http://localhost:8000/", :params => {:key => :thing })
+value = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+st = Time.now
+100_000.times do |i|
+  key = "#{Time.now.to_f.to_s}#{rand(1000)}"
+  r = `curl -X POST http://localhost:8000/ -d "#{key}=#{value}"`
+  puts "#{i} = #{key} = #{r}"
+  #response = Typhoeus::Request.post("http://localhost:8000/", :params => {key => value })
+end
+et = Time.now
+
+puts (et - st).to_f
