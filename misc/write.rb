e81c29ea6ae77b2145f35e6b72b8629c53d47cd4
Julio Capote <jcapote@gmail.com>
2011-11-03 22:02:01 -0700

    misc changes

--- misc/write.rb
+++ misc/write.rb
@@ -1,8 +1,3 @@
-require 'rubygems'
-require 'typhoeus'
-
-
-#response = Typhoeus::Request.get("http://localhost:8000/", :params => {:key => :thing })
 value = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 st = Time.now
