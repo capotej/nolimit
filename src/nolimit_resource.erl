bbfe680b7225605093f7d48b0bc5cab0078e8840
Julio Capote <jcapote@gmail.com>
2011-11-05 17:25:02 -0700

    using a record in ctx to pass stuff around

--- src/nolimit_resource.erl
+++ src/nolimit_resource.erl
@@ -8,28 +8,33 @@
 -include_lib("webmachine/include/webmachine.hrl").
 -include_lib("bitcask/include/bitcask.hrl").
 
-init(Config) -> 
-  {ok, Config}.
+-record(context, {bc=undefined, value=undefined}).
+
+init(_) -> 
+  Ctx = #context{bc=bitcask:open("nolimit.cask")},
+  {ok, Ctx}.
 
 allowed_methods(RD, Ctx) ->
-    {['GET', 'HEAD', 'POST'], RD, Ctx}.
+  {['GET', 'HEAD', 'POST'], RD, Ctx}.
 
 content_types_provided(RD, Ctx) ->
-    {[{"application/json", to_json}], RD, Ctx}.
+  {[{"application/json", to_json}], RD, Ctx}.
+
+finish_request(RD, Ctx) ->
+  bitcask:close(Ctx#context.bc).
 
 to_json(RD, Ctx) ->
-    [{"key",Key}] = wrq:req_qs(RD),
-    Bitcask = bitcask:open("nolimit.cask"),
-    Result = bitcask:get(Bitcask, term_to_binary(Key)),
-    bitcask:close(Bitcask),
-    case Result of
-      not_found -> {"not found", RD, Ctx};
-      {ok, Bin} -> {binary_to_term(Bin), RD, Ctx};
-      true -> {"error", RD, Ctx}
-    end.
+  Bitcask = Ctx#context.bc,
+  [{"key",Key}] = wrq:req_qs(RD),
+  Result = bitcask:get(Bitcask, term_to_binary(Key)),
+  case Result of
+    not_found -> {"not found", RD, Ctx};
+    {ok, Bin} -> {binary_to_term(Bin), RD, Ctx};
+    true -> {"error", RD, Ctx}
+  end.
 
 process_post(RD, Ctx) ->
-    [{Key,Value}] = mochiweb_util:parse_qs(wrq:req_body(RD)),
-    writer ! {write, Key, Value},
-    {true, wrq:append_to_response_body("ok", RD), Ctx}.
+  [{Key,Value}] = mochiweb_util:parse_qs(wrq:req_body(RD)),
+  writer ! {write, Key, Value},
+  {true, wrq:append_to_response_body("ok", RD), Ctx}.
 
